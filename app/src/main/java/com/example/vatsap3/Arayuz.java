e4727928da6de4da6c1a811cf7459e3ac68895ff
sametcl2 <34509901+sametcl2@users.noreply.github.com>
2019-06-06 21:48:24 +0300

    db eklendi

--- app/src/main/java/com/example/vatsap3/Arayuz.java
+++ app/src/main/java/com/example/vatsap3/Arayuz.java
@@ -4,22 +4,36 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arayuz extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private DatabaseReference databaseReference;
+    ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,38 +41,64 @@ public class Arayuz extends AppCompatActivity {
         setContentView(R.layout.activity_arayuz);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        databaseReference=FirebaseDatabase.getInstance().getReference();
+        arrayList=new ArrayList<>();
         recyclerView=findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         layoutManager=new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        adapter=new Adapter();
         recyclerView.setAdapter(adapter);
+
+        ValueEventListener valueEventListener=new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String adSoyad=dataSnapshot.getValue(User.class).adSoyad;
+                arrayList.add(adSoyad);
+                System.out.println(adSoyad);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+
+        databaseReference.addValueEventListener(valueEventListener);
+
+        adapter=new Adapter(arrayList);
     }
 }
 
 class Adapter extends RecyclerView.Adapter<Adapter.viewHolder>{
 
+    private ArrayList<String> data;
+
     public static class viewHolder extends RecyclerView.ViewHolder{
-        public TextView textView;
-        public viewHolder(@NonNull TextView itemView) {
+        TextView textView;
+        public viewHolder(@NonNull View itemView) {
             super(itemView);
-            textView=itemView;
+            textView=itemView.findViewById(R.id.textView);
         }
+    }
+
+    Adapter(ArrayList<String> data){
+        this.data=data;
     }
 
     @NonNull
     @Override
     public Adapter.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View item= LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view, parent, false);
+        return new Adapter.viewHolder(item);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Adapter.viewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        holder.textView.setText(data.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 }
